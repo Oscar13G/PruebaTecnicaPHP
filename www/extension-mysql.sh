baa16dbd4dfeb982c5311155051451e0467de472
Oscar13G <oscar.glz.ing@gmail.com>
2023-09-18 22:53:07 -0600

    Create extension-mysql.sh
    Add Bash extension MySQL

---
+++ www/extension-mysql.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo “Agregando extension PHP para MySQL”
+
+#Instalar extension
+#docker-php-ext-install mysqli
+
+#Directorio php init 
+cd /usr/local/etc/php
+
+#Modificar Archivos 
+sed -i 's/;   extension=\/path\/to\/extension\/mysqli.so/   extension=\/usr\/local\/lib\/php\/extensions\/no-debug-non-zts-20190902\/mysqli.so/' php.ini-development php.ini-production
+
+echo “Completado”
